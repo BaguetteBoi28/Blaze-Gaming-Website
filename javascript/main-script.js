697c0c0775121f39d79ed331bd03e497e982486b
BaguetteBoi28 <125702853+BaguetteBoi28@users.noreply.github.com>
2025-06-07 18:07:37 +0200

    Added shouldShowNewsletter function

--- javascript/main-script.js
+++ javascript/main-script.js
@@ -1,8 +1,22 @@
 const newsletterPrompt = document.getElementById("newsletter");
+const newsletterButton = document.getElementById("btnDelete");
+
+const newsletterStorageKey = 'bannerClosedAt';
+const newsletterHideDuration = 5 * 60 * 1000;
 
 function removeNewsletter() {
     newsletterPrompt.classList.add("after");
-    setTimeout(() => {
-        newsletterPrompt.remove();
-    },1100);
+    localStorage.setItem(newsletterStorageKey,Date.now());
+}
+
+function shouldShowNewsletter() {
+    const closedAt = localStorage.getItem(newsletterStorageKey);
+    if (!closedAt) return true;
+
+    const timePassed = Date.now() - parseInt(closedAt,10);
+    return timePassed > newsletterHideDuration;
+}
+
+if (!shouldShowNewsletter()) {
+    removeNewsletter();
 }
